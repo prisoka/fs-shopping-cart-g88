430716e86c7af7d68d5ceaa8fb79fb936d093d44
Priscilla Okawa <prisoka@gmail.com>
2018-05-30 11:59:28 -0700

    added possible solution, but can't pass the tests

--- src/main.js
+++ src/main.js
@@ -22,8 +22,20 @@ function getItem (path, idx) {
   }
 }
 
+// OUTPUT : updateItem(filePath, 1, { name: 'Beet', quantity: 8 })
 function updateItem (path, idx, item) {
-  
+  // if the item doesn't exist, stop.
+  if (!item.name || !item.quantity) return
+  if (!getItem(path,idx)) return
+
+  // get the single item assigning getItem to current variable:
+  const current = fs.readFileSync(path, 'utf-8').split('\n')
+  // Assing output to text variable:
+  const text = `${idx}\t${item.name}\t${item.quantity}\n`
+  current[idx-1] = text
+  // update file and join current using fs.writeFile() method, which replaces the specified file and content:
+  fs.writeFileSync(path, current.join('\n'))
+  }
 }
 
 module.exports = {
